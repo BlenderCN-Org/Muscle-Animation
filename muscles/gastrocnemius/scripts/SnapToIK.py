590142f1af461e4783d9d9502ebe2f6f1184d1dd
Matthew Varble <mrvarble@gmail.com>
2016-08-12 08:50:07 -0700

    Finished rig!

--- muscles/gastrocnemius/scripts/SnapToIK.py
+++ muscles/gastrocnemius/scripts/SnapToIK.py
@@ -9,13 +9,19 @@ ik_target = bpy.context.active_pose_bone
 # get the corresponding armature
 armature = ik_target.id_data.pose
 
-# find out which finger we are controlling
-side = ik_target.name[-1]
+# find out which IK chain we are controlling
+if "finger-ik." in ik_target.name:
+    ik_id = "MCH-finger-fk." + ik_target.name[-1]
+    fk_start = -8
+else:
+    ik_id = "MCH-tail-fk"
+    fk_start = -6
+
 
 # move all FK bones to the current IK orientation
 for bone in armature.bones:
-    if "MCH-finger-fk." + side in bone.name:
-        ik_name = bone.name[:11] + "ik" + bone.name[13:]
+    if ik_id in bone.name:
+        ik_name = bone.name[:fk_start] + "ik" + bone.name[fk_start+2:]
         ik_bone = armature.bones[ik_name]
         bpy.ops.pose.visual_transform_apply()
         bone.matrix = ik_bone.matrix
